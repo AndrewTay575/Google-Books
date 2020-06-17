32fb9ab2f116b5c0491477534b3518b8d46c2934
Andrew Taylor <andytaylor575@gmail.com>
2020-06-17 12:18:22 -0400

    Setting up Book modals

--- googlebooks/src/BookCard.jsx
+++ googlebooks/src/BookCard.jsx
@@ -1,3 +1,13 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {Card, CardTitle, CardImg, CardBody, Button, Modal} from 'reactstrap'
+const BookCard = ({ thumbnail }) => {
+
+    const [modal, setModal] = useState(false);
+    const toggle = () => setModal(!modal);
+    return <Card style={ {width: '233px' } } className = 'm-auto'>
+        <CardImg top atyle = {{width: '100%', height: '233px' }} src={thumbnail} alt = 'card image'/>
+    </Card>
+};
+
 
 export default BookCard
